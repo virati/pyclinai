5f5102a4e5ac909e86afa6fdba4080093b658f72
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2026-01-13 20:03:36 -0800

    removed cpt and condensed note gen

--- notebooks/demo_clin_notes.py
+++ notebooks/demo_clin_notes.py
@@ -12,23 +12,20 @@ def main():
     lm = dspy.LM("gemini/gemini-2.5-flash", api_key=GEMINI_KEY, max_tokens=10000)
     dspy.configure(lm=lm)
 
-    gen_note(
+    one_liners = [
         "PATIENT is a 55 yo Male smoker with no history of cancer complaining of a new tremor and cough.",
-        # updrs_score="UPDRS: 0",
-        complexity=1,
-        display_note=True,
-    )
-    # gen_note(
-    #     "PATIENT is a 23 yo Male smoker with no history of cancer complaining of a new tremor and chest pain.",
-    #     # updrs_score="UPDRS: 15",
-    #     complexity=1,
-    # )
+        "PATIENT is a 23 yo Male smoker with no history of cancer complaining of a new tremor and chest pain.",
+        "PATIENT is a 23 yo Male smoker with no history of cancer complaining of a new tremor and chest pain.",
+    ]
+    complexity_level = 5
 
-    # gen_note(
-    #     "PATIENT is a 23 yo Male smoker with no history of cancer complaining of a new tremor and chest pain.",
-    #     # updrs_score="UPDRS: 15",
-    #     complexity=9,
-    # )
+    for one_liner in one_liners:
+        gen_note(
+            one_liner,
+            # updrs_score="UPDRS: 0",
+            complexity=complexity_level,
+            display_note=True,
+        )
 
 
 if __name__ == "__main__":
