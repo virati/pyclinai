856eec1f097c6d63466a59bb6774411ea1a2e5df
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2026-02-01 08:18:29 -0800

    added disease state tracking

--- src/pyclinai/synthetic/behavior/base.py
+++ src/pyclinai/synthetic/behavior/base.py
@@ -1,4 +1,5 @@
 import dspy
+from typing import List
 
 
 class DzState(dspy.Signature):
@@ -9,8 +10,15 @@ class DzState(dspy.Signature):
     perplexity: float = dspy.InputField(
         desc="A number between 0 and 1 indicating how typical the presentation is for the disease. 0 means the vignette should be very stereotypical, while 1 means the vignette should be a rare, Zebra presentation."
     )
+    ###
     one_liner: str = dspy.OutputField(
         desc="A concise one-liner summary of the patient's presentation, including key symptoms, the Chief Complains, and brief relevant context."
+    )
+    symptoms: List[str] = dspy.OutputField(
+        desc="List of symptoms that the patient is feeling for this disease x progression x perplexity."
+    )
+    symptom_severities: List[float] = dspy.OutputField(
+        desc="Severity of each of the symptoms listed in symptoms, on a scale from 0 to 1."
     )
     clinical_scale: str = dspy.OutputField(
         desc="A clinical scale relevant to the disease, if applicable. This should include the name of the scale."
